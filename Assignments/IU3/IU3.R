0f5ab52e2f68518604d47a27fb0b1b71e20ffef0
alexanderbergkvist <alexander.bergkvist@hotmail.com>
2020-04-21 21:04:58 +0200

    j

--- Assignments/IU3/IU3.R
+++ Assignments/IU3/IU3.R
@@ -3,9 +3,18 @@ plot.ts(df)
 acf(df, plot = TRUE)
 pacf(df, plot = TRUE)
 
+l_df <- log(df)
+plot.ts(l_df)
+
 
 d_df <- diff(df)
 plot.ts(d_df)
+var(d_df)
+
+dl_df <- diff(l_df)
+plot.ts(dl_df)
+var(dl_df)
 acf(d_df, plot = TRUE)
 pacf(d_df, plot = TRUE)
 
+a = arima(dl_df, order = c(1, 0, 0s))
